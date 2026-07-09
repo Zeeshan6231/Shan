29065908b249fe2d911548b4906656e0888cb7e4
Muhammad Zeeshan <mzieshan01@gmail.com>
2026-07-09 15:03:12 +0500

    added welcome page content

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Currently lerning Mern Stack at Machine learning Bench
-        </p>
+        <h1>welcome!</h1>
+        <p><strong>currently learning:</strong></p>
+        <ul>
+          <li>✅ html</li>
+          <lI>✅ css</lI>
+          <li>✅ Javascript</li>
+          <li>✅ Reacts</li>
+          <li>✅ node.js and experess</li>
+          <li>✅ MONGODB</li>
+          </ul>
+          <p><em>Every expert was once a beginner</em></p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > Explore My journey
         </a>
       </header>
     </div>
