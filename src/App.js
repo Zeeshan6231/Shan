9907b000fe9e676688fd52b646de3f0d77b727f8
Muhammad Zeeshan <mzieshan01@gmail.com>
2026-07-09 13:52:30 +0500

    fixed export error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Currently lerning Mern Stack at Machine learning Bench
         </p>
         <a
           className="App-link"
